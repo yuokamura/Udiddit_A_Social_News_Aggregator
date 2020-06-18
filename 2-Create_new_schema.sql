fcc8faf50b5361f5889283da6e57d297a60f34a9
yuokamura <57396267+yuokamura@users.noreply.github.com>
2020-06-18 16:35:56 +0200

    Add files via upload

---
+++ 2-Create_new_schema.sql
@@ -0,0 +1,66 @@
+-- building schema
+
+CREATE TABLE "users" (
+  "id" SERIAL PRIMARY KEY,
+  "username" VARCHAR(25) UNIQUE NOT NULL,
+  "last_log_in" TIMESTAMP WITH TIME ZONE
+);
+CREATE INDEX "last_log_in" ON "users" ("last_log_in"); -- 2.a
+CREATE INDEX "find_username_pattern_matching"
+ON "users" ("username" VARCHAR_Pattern_OPS); --2.c
+--2.b users.id is pk so no indexing required
+
+
+CREATE TABLE "topics" (
+  "id" SERIAL PRIMARY KEY,
+  "name" VARCHAR(30) UNIQUE NOT NULL,
+  "description" VARCHAR(500)
+);
+CREATE INDEX "find_topic_pattern_matching"
+ON "topics" ("name" VARCHAR_Pattern_OPS); -- 2.e
+-- 2.d topics.id is pk so no indexing required
+
+
+CREATE TABLE "posts" (
+  "id" SERIAL PRIMARY KEY,
+  "topic_id" INTEGER REFERENCES "topics" ON DELETE CASCADE,
+  "user_id" INTEGER REFERENCES "users" ON DELETE SET NULL,
+  "post_date" TIMESTAMP WITH TIME ZONE,
+  "title" VARCHAR(100) NOT NULL,
+  "url" VARCHAR(4000) DEFAULT NULL,
+  "text_content" TEXT DEFAULT NULL,
+  CHECK ("url" IS NULL AND "text_content" IS NOT NULL
+        OR "url" IS NOT NULL AND "text_content" IS NULL)
+);
+CREATE INDEX "find_latest_post_per_topic"
+ON "posts" ("topic_id","post_date"); -- 2.f
+CREATE INDEX "find_latest_post_per_user"
+ON "posts" ("user_id","post_date"); -- 2.g
+CREATE INDEX "find_url_pattern_matching"
+ON "posts" ("url" VARCHAR_Pattern_OPS); --2.h
+
+
+CREATE TABLE "comments" (
+  "id" SERIAL PRIMARY KEY,
+  "post_id" INTEGER REFERENCES "posts" ON DELETE CASCADE,
+  "user_id" INTEGER REFERENCES "users" ON DELETE SET NULL,
+  "comment_date" TIMESTAMP WITH TIME ZONE,
+  "text_content" TEXT NOT NULL,
+  "parent_id" INTEGER DEFAULT NULL,
+  FOREIGN KEY ("parent_id") REFERENCES "comments" ON DELETE CASCADE
+  -- allows the thread system.
+);
+CREATE INDEX "find_parent_or_its_child_comment"
+ON "comments" ("parent_id"); -- 2.i and 2.j
+CREATE INDEX "find_latest_comment_per_user"
+ON "comments" ("user_id","comment_date"); -- 2.k
+
+
+CREATE TABLE "votes" (
+  "user_id" INTEGER REFERENCES "users" ON DELETE SET NULL,
+  "post_id" INTEGER REFERENCES "posts" ON DELETE CASCADE,
+  "vote" INTEGER CHECK("vote" = 1 OR "vote"=-1),
+  PRIMARY KEY ("post_id","user_id")
+);
+CREATE INDEX "scoring_post"
+ON "votes" ("vote"); -- 2.l
---
+++ 3-mirgrating_data.sql
@@ -0,0 +1,101 @@
+/* migrate all the user details */
+
+INSERT INTO "users" ("username")
+  SELECT DISTINCT "username"
+  FROM "bad_posts";  -- users who have made posts
+
+INSERT INTO "users" ("username")
+  SELECT DISTINCT "username"
+  FROM "bad_comments"
+  WHERE "username" NOT IN (
+    SELECT "username"
+    FROM "bad_posts"); -- users who have made comment only
+
+INSERT INTO "users" ("username")
+  SELECT  DISTINCT tab1."upvote_user"
+  FROM    (
+          SELECT REGEXP_SPLIT_TO_TABLE("upvotes",',') AS "upvote_user"
+          FROM  "bad_posts"
+          ) AS tab1
+  WHERE   tab1."upvote_user" NOT IN (
+            SELECT "username"
+            FROM   "users"); -- users who have only upvoted
+
+INSERT INTO "users" ("username")
+  SELECT  DISTINCT tab1."downvote_user"
+  FROM    (
+          SELECT REGEXP_SPLIT_TO_TABLE("downvotes",',') AS "downvote_user"
+          FROM  "bad_posts"
+          ) AS tab1
+  WHERE   tab1."downvote_user" NOT IN (
+            SELECT "username"
+            FROM   "users"); -- users who have only downvoted
+
+
+/* migrate all the topics and respective users */
+INSERT INTO "topics" ("name")
+  SELECT  DISTINCT  "topic"
+  FROM  "bad_posts";
+
+
+/* migrate posts from bad_posts */
+INSERT INTO "posts"
+("id","topic_id","user_id","title","url","text_content")
+  SELECT  bp."id",
+          t."id",
+          u."id",
+          bp."title",
+          bp."url",
+          bp."text_content"
+  FROM    "bad_posts" bp
+    JOIN  "topics" t
+    ON    bp."topic" = t."name"
+    JOIN  "users" u
+    ON    bp."username" = u."username"
+  WHERE   LENGTH(bp."title")<=100;
+  -- post with title longer than limited did not get migrated.
+
+
+/* migrate comments from bad_comments */
+INSERT INTO "comments"
+("post_id","user_id","text_content")
+  SELECT  p."id",
+          u."id",
+          bc."text_content"
+  FROM    "bad_comments" bc
+    JOIN  "posts" p
+    ON    bc."post_id" = p."id"
+    JOIN  "users" u
+    ON    bc."username" = u."username";
+
+
+/* migrate votes */
+-- migrate all upvotes as 1
+INSERT INTO "votes"
+("user_id","post_id","vote")
+  SELECT  u."id",
+          tab1."post_id",
+          1 AS vote
+  FROM    (
+          SELECT  "id" AS post_id,
+                  REGEXP_SPLIT_TO_TABLE(bp."upvotes",',') AS upvote_user
+          FROM    "bad_posts" bp
+          WHERE   LENGTH(bp."title")<=100
+          ) AS tab1
+  JOIN  users u
+  ON    tab1.upvote_user = u."username";
+
+-- migrate all downvotes as -1
+INSERT INTO "votes"
+("user_id","post_id","vote")
+  SELECT  u."id",
+          tab1."post_id",
+          -1 AS vote
+  FROM    (
+          SELECT  "id" AS post_id,
+                  REGEXP_SPLIT_TO_TABLE(bp."downvotes",',') AS downvote_user
+          FROM    "bad_posts" bp
+          WHERE   LENGTH(bp."title")<=100
+          ) AS tab1
+  JOIN  users u
+  ON    tab1.downvote_user = u."username";
